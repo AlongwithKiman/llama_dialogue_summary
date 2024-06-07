90fc439aba9b0d7f6751e2f8f13aa27a7dc3b47b
AlongwithKiman <expfunsce@snu.ac.kr>
2024-06-07 14:30:45 +0900

    fix: fstring bug fixed

--- inference/inference.py
+++ inference/inference.py
@@ -75,7 +75,8 @@ if __name__ == "__main__":
     test_data = test_data[:num_test_data]
     result = []
     for _data in test_data:
-        prompt = f"[INST]아래 대화를 한줄로 요약해주세요. \n\n### 대화:{_data["dialogue"]}\n[/INST]\n\n\n### 요약:"
+        _dialogue = _data["dialogue"]
+        prompt = f"[INST]아래 대화를 한줄로 요약해주세요. \n\n### 대화:{_dialogue}\n[/INST]\n\n\n### 요약:"
         model_input = tokenizer(prompt, return_tensors="pt").to("cuda")
 
 
